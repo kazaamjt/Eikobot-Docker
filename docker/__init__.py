c2bed784a3bcc14b4e28bb06eacaa90a37d2956c
kazaamjt <kazaamjt@gmail.com>
2023-06-05 15:48:16 +0200

    Add exposing of docker http API

--- docker/__init__.py
+++ docker/__init__.py
@@ -3,11 +3,14 @@ Models for deploying and managing Docker on a remote host.
 """
 from pathlib import Path
 
+import aiohttp
 from eikobot.core.handlers import CRUDHandler, HandlerContext
 from eikobot.core.helpers import EikoBaseModel
 from eikobot.core.lib.std import CmdResult, HostModel
+from eikobot.core.lib.std.file import FileModel
 
-_INSTALL_SCRIPT_DIR = Path(__file__).parent / "install_scripts"
+_FILES_DIR = Path(__file__).parent / "files"
+_HTTP_TIMEOUT = aiohttp.ClientTimeout(10)
 
 
 async def _verify_install(ctx: HandlerContext, host: HostModel) -> bool:
@@ -49,7 +52,7 @@ class DockerInstallerHandler(CRUDHandler):
         return await _verify_install(ctx, host)
 
     async def _install_debian(self, ctx: HandlerContext, host: HostModel) -> CmdResult:
-        script = (_INSTALL_SCRIPT_DIR / "debian.sh").read_text()
+        script = (_FILES_DIR / "debian.sh").read_text()
         return await host.script(script, "bash", ctx)
 
     async def create(self, ctx: HandlerContext) -> None:
@@ -72,3 +75,88 @@ class DockerInstallerHandler(CRUDHandler):
             return
 
         ctx.deployed = True
+
+
+class DockerHostModel(EikoBaseModel):
+    """
+    Model that represents a docker installation
+    perfromed by Eikobot.
+    """
+
+    __eiko_resource__ = "DockerHost"
+
+    host: HostModel
+    install: bool
+    docker_port: int = 2376
+
+
+class DockerHostHandler(CRUDHandler):
+    """
+    Configures docker on a remote host.
+    """
+
+    __eiko_resource__ = "DockerHost"
+
+    async def __pre__(self, ctx: HandlerContext) -> None:
+        if not isinstance(ctx.resource, DockerHostModel):
+            ctx.failed = True
+            return
+
+        if ctx.resource.host.host not in ["127.0.0.1", "localhost"]:
+            await ctx.resource.host.forward_port(
+                ctx, ctx.resource.docker_port, ctx.resource.docker_port
+            )
+
+    async def create(self, ctx: HandlerContext) -> None:
+        if not isinstance(ctx.resource, DockerHostModel):
+            ctx.failed = True
+            return
+
+        await ctx.resource.host.execute(
+            "sudo systemctl daemon-reload",
+            ctx,
+        )
+        await ctx.resource.host.execute(
+            "sudo systemctl restart docker",
+            ctx,
+        )
+
+        async with aiohttp.ClientSession(timeout=_HTTP_TIMEOUT) as session:
+            response = await session.get(
+                f"http://127.0.0.1:{ctx.resource.docker_port}/info"
+            )
+            if response.status == 200:
+                ctx.deployed = True
+
+    async def read(self, ctx: HandlerContext) -> None:
+        if not isinstance(ctx.resource, DockerHostModel):
+            ctx.failed = True
+            return
+
+        if ctx.resource.host.os_platform.resolve(str) != "linux-gnu":
+            ctx.error(
+                f"OS '{ctx.resource.host.os_platform.resolve(str)}' is currently not supported."
+            )
+            ctx.failed = True
+            return
+
+        async with aiohttp.ClientSession(timeout=_HTTP_TIMEOUT) as session:
+            try:
+                response = await session.get(
+                    f"http://127.0.0.1:{ctx.resource.docker_port}/info"
+                )
+                if response.status == 200:
+                    ctx.deployed = True
+            except aiohttp.ClientConnectionError:
+                return
+
+    async def __post__(self, ctx: HandlerContext) -> None:
+        if not isinstance(ctx.resource, DockerHostModel):
+            ctx.failed = True
+            return
+
+        if ctx.resource.host.host not in ["127.0.0.1", "localhost"]:
+            await ctx.resource.host.stop_forwarding_port(
+                ctx,
+                ctx.resource.docker_port,
+            )
